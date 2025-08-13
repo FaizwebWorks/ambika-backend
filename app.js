70eaa670797b8f4673de1319ce6a3a1879cffce2
Faizal Shaikh <webworksbyfaiz@gmail.com>
2025-08-13 15:11:12 +0530

    feat: Update CORS configuration to allow multiple origins

--- app.js
+++ app.js
@@ -22,7 +22,11 @@ connectDB();
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: [
+    process.env.FRONTEND_URL || "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:3000"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true
 }));
